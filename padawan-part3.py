891402ec4aee4abde2fbc4707a55893b188cbb79
rui1610 <rui.nogueira1610@hotmail.com>
2021-07-08 15:06:42 +0000

    add meaningfull output

--- padawan-part3.py
+++ padawan-part3.py
@@ -1,3 +1,34 @@
+from pptx import Presentation
+from pptx.util import Inches
+from pptx.util import Pt
 
-print "HELLO PADAWAN!"
+prs = Presentation()
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+
+title.text = "Hello, my very dear Padawan!"
+subtitle.text = "I can feel The Force is strong inside of you!"
+
+left =  Inches(1)
+top = Inches(6)
+width = Inches(5)
+height = Inches(1)
+textBox = slide.shapes.add_textbox(left, top, width, height)
+
+tf = textBox.text_frame
+tf.text = "But never forget:"
+tf.paragraphs[0].font.size = Pt(20)
+tf.paragraphs[0].font.bold = True
+
+p = tf.add_paragraph()
+run = p.add_run()
+run.text = "Fear leads to the dark side of The Force!"
+run.font.size = Pt(20)
+run.hyperlink.address = "https://www.youtube.com/watch?v=kFnFr-DOPf8"
+
+filename = 'The force inside of you is strong.pptx'
+prs.save(filename)
+
 
